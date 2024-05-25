1991db7c6797da4489aca1e0d03f76531888b765
Josue Gonzalez <Josuegonzalezd046@gmail.com>
2024-05-25 20:17:32 +0000

    completed typescript arrays exercise.

---
+++ typescript-arrays/main.js
@@ -0,0 +1,18 @@
+'use strict';
+const colors = ['red', 'white', 'blue'];
+console.log('value of colors[0]: ', colors[0]);
+console.log('value of colors[1]: ', colors[1]);
+console.log('value of colors[2]: ', colors[2]);
+console.log(`America is ${colors[0]},  ${colors[1]}, and ${colors[2]}`);
+colors[2] = 'green';
+console.log(`Mexico is ${colors[0]}, ${colors[1]}, and ${colors[2]}`);
+console.log('Value of the colors array: ', colors);
+console.log('Typeof colors: ', typeof colors);
+const students = ['Josue', 'Lydia', 'Isaiah', 'Lil Josue'];
+const numberOfStudents = students.length;
+console.log(`There are ${numberOfStudents} students in the class.`);
+const lastIndex = numberOfStudents - 1;
+const lastStudent = students[lastIndex];
+console.log(`The last student of the array is ${lastStudent}`);
+console.log('value of students: ', students);
+console.log('typeof students: ', typeof students);
